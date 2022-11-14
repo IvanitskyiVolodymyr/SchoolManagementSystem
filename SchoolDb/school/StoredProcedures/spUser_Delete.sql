180676918ba7ac2c813d190ca32d172de97c67c0
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-11-14 11:37:04 +0200

    Created stored procedured for User CRUD operations

---
+++ SchoolDb/school/StoredProcedures/spUser_Delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Delete]
+	@UserId int
+AS
+BEGIN
+	DELETE 
+	FROM school.Users
+	WHERE UserId = @UserId
+END
---
+++ SchoolDb/school/StoredProcedures/spUser_Insert.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	@UserId INT,
+    @FirstName NVARCHAR (20),
+    @RoleId INT,
+    @MiddleName NVARCHAR (20),
+    @LastName NVARCHAR (20),
+    @Gender NVARCHAR (1),
+    @PhoneNumber NVARCHAR (20),
+    @Email NVARCHAR (20),
+    @Address NVARCHAR (100),
+    @PasswordSalt NVARCHAR (100),
+    @BirthDate DATE,
+    @JoinDate DATE,
+    @AvatarUrl NVARCHAR (255)
+AS
+BEGIN
+    INSERT INTO school.Users
+    (UserId,
+    FirstName,
+    RoleId,
+    MiddleName,
+    LastName,
+    Gender,
+    PhoneNumber,
+    Email,
+    Address,
+    PasswordSalt,
+    BirthDate,
+    JoinDate,
+    AvatarUrl)
+    VALUES
+    (@UserId,
+    @FirstName,
+    @RoleId,
+    @MiddleName,
+    @LastName,
+    @Gender,
+    @PhoneNumber,
+    @Email,
+    @Address,
+    @PasswordSalt,
+    @BirthDate,
+    @JoinDate,
+    @AvatarUrl);
+END
