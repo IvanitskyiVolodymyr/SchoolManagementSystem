df9536804c1dce9e118357ba0d6147d34791ff15
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-11-16 14:04:22 +0200

    Added hash field to User table

--- Domain.Core/Entities/User.cs
+++ Domain.Core/Entities/User.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Core.Models
+﻿
+namespace Domain.Core.Entities
 {
     public class User
     {
@@ -17,6 +12,7 @@ namespace Domain.Core.Models
         public string Email { get; set; }
         public string Address { get; set; }
         public string PasswordSalt { get; set; }
+        public string PasswordHash { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime JoinDate { get; set; }
         public string AvatarUrl { get; set; }
