d8561cc2457c53c5e2b7df817d97473a092af8df
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-11-23 11:26:04 +0200

    Creating a parent only with students Id is limited

--- Domain.Interfaces/Repositories/IParentRepository.cs
+++ Domain.Interfaces/Repositories/IParentRepository.cs
@@ -5,5 +5,6 @@ namespace Domain.Interfaces.Repositories
     public interface IParentRepository
     {
         Task<int> CreateParent(InsertParentDto parentDto);
+        Task<int> CreateParentStudent(int parentId, int studentId);
     }
 }
---
+++ SchoolDb/school/StoredProcedures/ParentStudent/spParentStudent_Insert.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spParentStudent_Insert]
+	@ParentId INT,
+	@StudentId INT
+AS
+BEGIN
+	INSERT INTO [school].[StudentsParents](ParentId, StudentId)
+	OUTPUT inserted.StudentParentId
+	VALUES (@ParentId, @StudentId)
+END
