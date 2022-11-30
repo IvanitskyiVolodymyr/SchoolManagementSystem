43ae36483946e887d9580a9f6c35575df2bd1f57
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-11-30 16:40:21 +0200

    Get all student subjects

--- Domain.Interfaces/Repositories/ISubjectRepository.cs
+++ Domain.Interfaces/Repositories/ISubjectRepository.cs
@@ -8,5 +8,6 @@ namespace Domain.Interfaces.Repositories
         Task<int> InsertSubject(InsertSubjectDto subjectDto);
         Task<IEnumerable<int>> InsertSubjects(IEnumerable<InsertSubjectDto> subjects);
         Task<IEnumerable<Subject>> GetAllSubjects();
+        Task<IEnumerable<StudentSubjectResponseDto>> GetSubjectsByStudentId(int studentId);
     }
 }
---
+++ SchoolDb/school/StoredProcedures/Subject/spSubject_GetAllByStudentId.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spSubject_GetAllByStudentId]
+	@StudentId INT
+AS
+BEGIN
+	SELECT S.SubjectId, S.Name, CS.ClassSubjectId FROM [school].[Subjects] S
+	JOIN [school].[ClassesSubjects] CS ON S.SubjectId = CS.SubjectId
+	JOIN [school].[Students] ST ON St.ClassId = CS.ClassId
+
+	WHERE ST.StudentId = @StudentId
+END
