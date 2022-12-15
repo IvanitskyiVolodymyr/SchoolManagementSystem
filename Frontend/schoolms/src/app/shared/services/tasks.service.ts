782923dd9a93ae7d28d5db2eec01e1c6c271fcf5
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-12-15 12:12:29 +0200

    fixed time bug

--- Frontend/schoolms/src/app/shared/services/tasks.service.ts
+++ Frontend/schoolms/src/app/shared/services/tasks.service.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
@@ -12,7 +13,8 @@ export class TasksService {
   
   private prefix = "/tasks";
   constructor(
-    private httpService: HttpClientService
+    private httpService: HttpClientService,
+    private datePipe: DatePipe
   ) { }
 
   public GetAllHomeworksForStudent(studentId: number, from: Date, to: Date) :Observable<Array<ResponseTask>>{
@@ -28,12 +30,15 @@ export class TasksService {
 
   public GetAllTasksWithGradesForStudent(studentId: number, from: Date, to: Date): Observable<Array<ResponseTaskWithGrade>> {
     return this.httpService.getFullRequest<Array<ResponseTaskWithGrade>>(`${this.prefix}/GetAllTasksWithGradesForStudent`,
-    new HttpParams().set('studentId', studentId).set('from', from.toDateString()).set('to', to.toDateString()))
-    .pipe(
-      map(
-        (resp) => {
-          return resp.body as Array<ResponseTaskWithGrade>;
-        })
-    )
+    new HttpParams()
+    .set('studentId', studentId)
+    .set('from', this.datePipe.transform(from, 'MMM d, y, h:mm:ss a') as string)
+    .set('to', this.datePipe.transform(to, 'MMM d, y, h:mm:ss a') as string))
+      .pipe(
+        map(
+          (resp) => {
+            return resp.body as Array<ResponseTaskWithGrade>;
+          })
+      )
   }
 }
