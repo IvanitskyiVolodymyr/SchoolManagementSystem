1ef8de2cc4f543e6a76a2af7f577e97ca1b46573
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-12-20 18:52:28 +0200

    added tests for ScheduleService

---
+++ Backend/Services.Tests.Unit/MockRepositories/MockAttendanceRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Core.Entities;
+using Domain.Interfaces.Repositories;
+using Moq;
+
+namespace Services.Tests.Unit.MockRepositories
+{
+    public class MockAttendanceRepository : Mock<IAttendanceRepository>
+    {
+        public void GetAttendancesForStudentByPeriod(DateTime startDateTime, DateTime endDateTime, int studentId, IEnumerable<Attendance> output)
+        {
+            Setup(x => x.GetAttendancesForStudentByPeriod
+                (It.Is<DateTime>(i => i == startDateTime),
+                It.Is<DateTime>(i => i == endDateTime),
+                It.Is<int>(i => i == studentId)))
+                .ReturnsAsync(output);
+        }
+    }
+}
