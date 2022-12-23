b854b4ed2426621ad45de8c00894c77a2080663a
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-12-23 15:32:22 +0200

    Show count of draft attachment

--- Frontend/schoolms/src/app/students/components/task-card/task-card.component.ts
+++ Frontend/schoolms/src/app/students/components/task-card/task-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { ResponseTaskWithGrade } from 'src/app/shared/models/tasks/responseTaskWithGrade';
+import { selectStudentTaskAttachments } from 'src/app/store/selectors/user.selector';
 
 @Component({
   selector: 'app-task-card',
@@ -10,13 +12,16 @@ import { ResponseTaskWithGrade } from 'src/app/shared/models/tasks/responseTaskW
 export class TaskCardComponent implements OnInit{
 
   @Input() task: ResponseTaskWithGrade = {} as ResponseTaskWithGrade;
+  public draftAttachments = 0;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private store: Store) { }
 
   public statusBgColor = '#E4F6D0';
 
   ngOnInit(): void {
     this.setStatusColor();
+    this.getStudentTaskAttachmentFromStorage(this.task.studentTaskId);
   }
 
   private setStatusColor() {
@@ -34,5 +39,15 @@ export class TaskCardComponent implements OnInit{
 
   public taskClick() {
     this.router.navigate(['/student/tasks/',this.task.studentTaskId]);
+  }
+
+  private getStudentTaskAttachmentFromStorage(studentTaskId: number) {
+    this.store.select(selectStudentTaskAttachments(studentTaskId)).subscribe(
+      result => {
+        if(result.length > 0 && result[0]?.attachments.length > 0) {
+          this.draftAttachments = result[0].attachments.length;
+        }
+      }
+    );
   }
 }
