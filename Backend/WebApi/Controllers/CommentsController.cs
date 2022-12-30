cc1fd1afd7c681a348724fd8b793631a58055b96
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2022-12-30 15:59:21 +0200

    Added time validation check for deleting and updating

--- Backend/WebApi/Controllers/CommentsController.cs
+++ Backend/WebApi/Controllers/CommentsController.cs
@@ -1,22 +1,26 @@
 ﻿using Application.Interfaces;
 using Common.Dtos.StudentTaskComment;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 using WebApi.Validators.Tasks;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class CommentsController : ControllerBase
     {
         private readonly TasksValidators _tasksValidators;
+        private readonly CommentsValidator _commentsValidators;
         private readonly ICommentService _commentService;
 
-        public CommentsController(TasksValidators tasksValidators, ICommentService commentService)
+        public CommentsController(TasksValidators tasksValidators, ICommentService commentService, CommentsValidator commentsValidators)
         {
             _tasksValidators = tasksValidators;
             _commentService = commentService;
+            _commentsValidators = commentsValidators;
         }
 
         [HttpGet("get-comments-by-student-task-id")]
@@ -35,14 +39,16 @@ namespace WebApi.Controllers
         [HttpPut("update-comment")]
         public async Task<ActionResult<ResponseStudentTaskCommentDto>> UpdateComment([FromBody] UpdateStudentTaskCommentDto comment)
         {
-            //add validation
+            await _commentsValidators.CheckAccessByStudentTaskCommentId(comment.StudentTaskCommentId);
+            await _commentsValidators.CheckUpdateDeletePermission(comment.StudentTaskCommentId);
             return Ok(await _commentService.UpdateComment(comment));
         }
 
         [HttpDelete("delete-comment")]
         public async Task<ActionResult<int>> DeleteComment(int studentTaskCommentId)
         {
-            //add validation
+            await _commentsValidators.CheckAccessByStudentTaskCommentId(studentTaskCommentId);
+            await _commentsValidators.CheckUpdateDeletePermission(studentTaskCommentId);
             return Ok(await _commentService.DeleteComment(studentTaskCommentId));
         }
     }
---
+++ Backend/WebApi/Validators/Tasks/CommentsValidator.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces;
+using Common.Dtos.StudentTaskComment;
+using Common.Exceptions;
+using Domain.Interfaces.Repositories;
+
+namespace WebApi.Validators.Tasks
+{
+    public class CommentsValidator
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IStudentTaskCommentRepository _commentRepository;
+
+        public CommentsValidator(IStudentTaskCommentRepository commentResository, ICurrentUserService currentUserService)
+        {
+            _commentRepository = commentResository;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task CheckAccessByStudentTaskCommentId(int studentTaskCommentId)
+        {
+            int currentUserId = _currentUserService.GetCurrentUserId();
+
+            var studentCommentTask = await _commentRepository.GetCommentsByStudentTaskCommentId(studentTaskCommentId);
+            if (studentCommentTask is null)
+            {
+                throw new NotFoundException(typeof(ResponseStudentTaskCommentDto), "StudentTaskCommentId", studentTaskCommentId.ToString());
+            }
+
+            if(currentUserId != studentCommentTask.ShortUserInfo.UserId)
+            {
+                throw new NotAcceptableException();
+            }
+        }
+
+        public async Task CheckUpdateDeletePermission(int studentTaskCommentId)
+        {
+            var studentCommentTask = await _commentRepository.GetCommentsByStudentTaskCommentId(studentTaskCommentId);
+            var dateDiff = DateTime.Now - studentCommentTask.CreatedAt;
+
+            if(dateDiff > TimeSpan.FromMinutes(5))
+            {
+                throw new ValidationException("You cannot delete or update a comment after 5 minutes from the time it was created");
+            }
+        }
+    }
+}
