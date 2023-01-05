3bea78841807888974b6e5b589a9d7fdd8e85a75
Ivanitskyi Volodymyr <volodymyr01it@gmail.com>
2023-01-05 10:33:38 +0200

    added teacher schedule card and teacher schedule list

--- Frontend/schoolms/src/app/teacher/teacher-routing.module.ts
+++ Frontend/schoolms/src/app/teacher/teacher-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotAcceptableComponent } from '../shared/components/errors/not-acceptable/not-acceptable.component';
 import { TeacherBaseComponent } from './components/teacher-base/teacher-base.component';
+import { TeacherScheduleListComponent } from './components/teacher-schedule-list/teacher-schedule-list.component';
 
 const teacherRoutes: Routes = [
   {
     path: '', component: TeacherBaseComponent,
     children: [
-
+      {
+        path: 'schedule', component: TeacherScheduleListComponent
+      },
       { path: 'not-acceptable', component: NotAcceptableComponent }
     ],
   },
--- Frontend/schoolms/src/app/teacher/teacher.module.ts
+++ Frontend/schoolms/src/app/teacher/teacher.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { TeacherRoutingModule } from './teacher-routing.module';
 import { TeacherBaseComponent } from './components/teacher-base/teacher-base.component';
 import { SharedModule } from '../shared/shared.module';
+import { TeacherScheduleListComponent } from './components/teacher-schedule-list/teacher-schedule-list.component';
+import { TeacherScheduleCardComponent } from './components/teacher-schedule-card/teacher-schedule-card.component';
 
 
 @NgModule({
   declarations: [
-    TeacherBaseComponent
+    TeacherBaseComponent,
+    TeacherScheduleListComponent,
+    TeacherScheduleCardComponent
   ],
   imports: [
     CommonModule,
